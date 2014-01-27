b21d325d5eedf7e34f8a2386e63f21319c78e8d5
Kasper Jacobsen <k@three65.org>
2014-01-27 19:28:35 +0100

    Initial job works

--- jobs/jobs.py
+++ jobs/jobs.py
@@ -1,8 +1,27 @@
 import time
 from datetime import datetime
-from redis import Redis
-r = Redis()
+import logging
+from redis import StrictRedis
+import requests
+
+logger = logging.getLogger('upup-job.logger')
+logger.setLevel(logging.INFO)
+requests_log = logging.getLogger('requests')
+requests_log.setLevel(logging.WARN)
+
+r = StrictRedis()
+
 def requestSite(site):
-	if r.hset(site['id'], site['last_run'], datetime.now()) > 0:
-		print 'Redis {}->last_run updated'.format(site['id'])
-	time.sleep(1)
+	if 'True' in site['active']:
+		logger.debug('JOB: {} site is active'.format(site['title']))
+		if r.hset(site['id'], 'last_run', datetime.now()) == 0:
+			logger.debug('JOB: {} -> last_run set to now(): {}'.format(site['title'], datetime.now()))
+			req = getattr(requests, site['method'].lower())
+			result = req(site['url'], timeout=int(site['timeout'])).elapsed
+			logger.debug('JOB: {} response tme is {}'.format(site['title'], result))
+			key = 'response:%s' % site['id']
+			if r.zadd(key, int(time.time()), result) > 0:
+				logger.info('JOB: {} metric added for {}'.format(site['title'], key))
+				return True
+			else:
+				logger.error('JOB: {} Zad add returned 0 - Metric not added for {}'.format(site['title'], key))
