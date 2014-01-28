3217dc3420c4f57c5c869b48d46a55d4dce002c7
Kasper Jacobsen <k@three65.org>
2014-01-28 22:27:08 +0100

    Testing test config

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from api.v1.views import api
 
 app = Flask(__name__)
 
-app.config['TEST'] = False
+app.config['TESTING'] = False
 
 app.config['REDIS_HOST'] = 'localhost'
 app.config['REDIS_PORT'] = 6379
-if app.config['TEST']:
+if app.config['TESTING']:
 	app.config['REDIS_DB'] = 1
 else:
 	app.config['REDIS_DB'] = 0
